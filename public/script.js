bf102f0bc48e386c749ce5cd38535c770389e757
Zawarudo1001 <timohinvaceslav4@gmail.com>
2026-05-05 13:20:43 +0300

    +++

--- public/script.js
+++ public/script.js
@@ -32,9 +32,7 @@ document.getElementById('flask-btn').addEventListener('click', async () => {
     
     responseText.textContent = data.message;
     responseText.style.color = '#4CAF50';
-  } catch (err) {
-    responseText.textContent = 'Ошибка связи с Flask';
-    responseText.style.color = '#f44336';
+
   } finally {
     btn.disabled = false;
     btn.textContent = 'Action';
